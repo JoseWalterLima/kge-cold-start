7c564774cdb21c5aa5a9eedf884be6b6d4d96b4e
JoseWalterLima <jwl.mota@gmail.com>
2025-10-25 17:08:55 -0300

    feat: adjust to ML-1M

--- src/data_splitter.py
+++ src/data_splitter.py
@@ -8,7 +8,7 @@ import requests
 import zipfile
 import io
 import pandas as pd
-data_url = "https://files.grouplens.org/datasets/movielens/ml-100k.zip"
+data_url = "https://files.grouplens.org/datasets/movielens/ml-1m.zip"
 directory = "data/"
 os.makedirs(directory, exist_ok=True)
 
@@ -21,93 +21,93 @@ def download_and_extract_movielens():
 download_and_extract_movielens()
 # Build Movies dataset
 m=pd.read_csv(
-    'data/ml-100k/u.item',
-    sep='|',
+    'data/ml-1m/movies.dat',
+    sep='::',
     encoding="latin1",
     names=[
-        'movieId', 'movieTitle', 'releaseDate', 'videoReleaseDate',
-        'IMDbURL', 'unknown', 'Action', 'Adventure', 'Animation',
-        'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama',
-        'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery',
-        'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western'
-        ]
-           ).drop(['unknown', 'videoReleaseDate', 'IMDbURL'], axis=1)
-# Reshaping
-m=m.melt(
-    id_vars=["movieId", "movieTitle", "releaseDate"],
-    var_name="genreDesc",
-    value_name="is_genre"
-    ).copy()
-m=m[m["is_genre"]==1].drop("is_genre", axis=1)\
-    .reset_index(drop=True).copy()
-# Extract features
-m['movieTitle']=m['movieTitle'].str[:-7]
-m['releaseDate']=m['releaseDate'].str[3:]
+        'movieId', 'movieTitle_Date', 'Genres']
+           )
+m['movieTitle']=m['movieTitle_Date'].str[:-7]
+m['releaseDate']=m['movieTitle_Date'].str[-5:-1]
 
-# Movie Nodes
-m[['movieId', 'movieTitle']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/movieNode.csv', index=False)
+print(m.tail(20))
 
-m[['releaseDate']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/releaseNode.csv', index=False)
+# # Reshaping
+# m=m.melt(
+#     id_vars=["movieId", "movieTitle", "releaseDate"],
+#     var_name="genreDesc",
+#     value_name="is_genre"
+#     ).copy()
+# m=m[m["is_genre"]==1].drop("is_genre", axis=1)\
+#     .reset_index(drop=True).copy()
+# # Extract features
+# m['movieTitle']=m['movieTitle'].str[:-7]
+# m['releaseDate']=m['releaseDate'].str[3:]
 
-m[['genreDesc']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/genreNode.csv', index=False)
+# # Movie Nodes
+# m[['movieId', 'movieTitle']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/movieNode.csv', index=False)
 
-# Movie Relationships
-m[['movieId', 'releaseDate']
-         ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
-            .to_csv('data/releaseRel.csv', index=False)
+# m[['releaseDate']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/releaseNode.csv', index=False)
 
-m[['movieId', 'genreDesc']
-         ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
-            .to_csv('data/genreRel.csv', index=False)
-# Keep only valid movies
-w=pd.read_csv('data/ml-100k/u.data', sep='\t',
-names=['userId', 'movieId', 'rating', 'timestamp'])[['userId', 'movieId']]
-w=w[w['movieId'].isin(m['movieId'].unique().tolist())]
-w.to_csv('data/watchedRel.csv', index=False)
+# m[['genreDesc']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/genreNode.csv', index=False)
 
-# User dataset
-u=pd.read_csv('data/ml-100k/u.user', sep='|',
-names=['userId', 'age', 'gender', 'occupation', 'zipcode'])
-# simplify location
-u['zipcode']=u['zipcode'].str[:-3]
+# # Movie Relationships
+# m[['movieId', 'releaseDate']
+#          ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
+#             .to_csv('data/releaseRel.csv', index=False)
 
-# User Nodes
-u[['userId']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/userNode.csv', index=False)
+# m[['movieId', 'genreDesc']
+#          ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
+#             .to_csv('data/genreRel.csv', index=False)
+# # Keep only valid movies
+# w=pd.read_csv('data/ml-100k/u.data', sep='\t',
+# names=['userId', 'movieId', 'rating', 'timestamp'])[['userId', 'movieId']]
+# w=w[w['movieId'].isin(m['movieId'].unique().tolist())]
+# w.to_csv('data/watchedRel.csv', index=False)
 
-u[['age']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/ageNode.csv', index=False)
+# # User dataset
+# u=pd.read_csv('data/ml-100k/u.user', sep='|',
+# names=['userId', 'age', 'gender', 'occupation', 'zipcode'])
+# # simplify location
+# u['zipcode']=u['zipcode'].str[:-3]
 
-u[['gender']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/genderNode.csv', index=False)
+# # User Nodes
+# u[['userId']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/userNode.csv', index=False)
 
-u[['occupation']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/occupationNode.csv', index=False)
+# u[['age']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/ageNode.csv', index=False)
 
-u[['zipcode']].copy().drop_duplicates()\
-    .dropna(how='all').reset_index(drop=True)\
-    .to_csv('data/zipcodeNode.csv', index=False)
+# u[['gender']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/genderNode.csv', index=False)
 
-# User Relationships
-u[['userId', 'age']
-         ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
-            .to_csv('data/ageRel.csv', index=False)
-u[['userId', 'gender']
-         ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
-            .to_csv('data/genderRel.csv', index=False)
-u[['userId', 'occupation']
-         ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
-            .to_csv('data/occupationRel.csv', index=False)
-u[['userId', 'zipcode']
-         ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
-            .to_csv('data/residesRel.csv', index=False)
+# u[['occupation']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/occupationNode.csv', index=False)
+
+# u[['zipcode']].copy().drop_duplicates()\
+#     .dropna(how='all').reset_index(drop=True)\
+#     .to_csv('data/zipcodeNode.csv', index=False)
+
+# # User Relationships
+# u[['userId', 'age']
+#          ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
+#             .to_csv('data/ageRel.csv', index=False)
+# u[['userId', 'gender']
+#          ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
+#             .to_csv('data/genderRel.csv', index=False)
+# u[['userId', 'occupation']
+#          ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
+#             .to_csv('data/occupationRel.csv', index=False)
+# u[['userId', 'zipcode']
+#          ].copy().drop_duplicates().dropna(how='all').reset_index(drop=True)\
+#             .to_csv('data/residesRel.csv', index=False)
